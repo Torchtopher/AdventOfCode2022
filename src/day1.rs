5a2d4b9d48dd4f0f3343a7477b352063af8d33cf
Torchtopher <torchtopher@gmail.com>
2022-12-02 13:43:46 -0500

    day1 done for any len

--- src/day1.rs
+++ src/day1.rs
@@ -1,8 +1,54 @@
-
+// use binary heap to sort the numbers
+//
+use std::collections::BinaryHeap;
 
 pub fn run(input: String) -> (i128, i128) {
+
     println!("Hello from day 1!");
-   
-    println!("input: {}", input);
-    (64, 64)
+    let size = 3;
+    let mut elf = 0;
+    // make binary heap of length 3
+    let mut heap: BinaryHeap<i128> = BinaryHeap::with_capacity(size);
+    let mut temp: Vec<i128> = Vec::with_capacity(size); // probably should be array instead of vec
+    // itterate over each line in the input
+    for line in input.lines() {
+        //println!("line: {}", line);
+        if line == "" {
+            //println!("Elf {} is being worked on", elf);
+            //println!("heap: {:?}", heap);
+            // tried to make super efficent  
+            if heap.len() == size {
+                for _ in 0..size {
+                    temp.push(heap.pop().unwrap());
+                    //println!("temp: {:?}", temp);
+                    //println!("heap: {:?}", heap);
+                    if elf > temp[temp.len()-1] { // will end up popping 3 times in average case
+                        heap.push(elf);
+                        // remove number from temp
+                        temp.pop();
+                        break;
+                    }
+                }
+                // re add vec to heap
+                for i in &temp {
+                    heap.push(i.clone());
+                }
+                temp.clear();
+            }
+            else {
+                heap.push(elf);
+            }
+            //println!("heap after: {:?}", heap);
+            elf = 0;
+        }
+        else {
+            // add the number to elf
+            elf += line.parse::<i128>().unwrap();
+        }
+    }
+    // println!("heap: {:?}", heap);
+    let part1 = heap.peek().unwrap().clone(); // probably cheap to clone a primitive type
+    // add up the numbers in the heap
+    let part2: i128 = heap.iter().sum();
+    return (part1, part2);  
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::collections::HashMap;
 use std::fs;
 use std::path;
 mod day1;
@@ -9,7 +8,7 @@ fn invalid_input(input: &String) {
     println!("Invalid argument \"{input}\", should be a day like day4");
 }
 
-pub fn read_input(day: i32) -> String {
+pub fn read_input(day: &i32) -> String {
     // make path dynamic based on day
     let path = path::Path::new("inputs").join(format!("day{}.txt", day));
     println!("Reading input from {:?}", path);
@@ -17,28 +16,52 @@ pub fn read_input(day: i32) -> String {
     input
 }
 
+
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     //dbg!(args);
     let dayarg: &String = &args[1];
-    let daystr: String = "day1".to_string();
-    // make a hashmap of string to function
-    let mut daymap: HashMap<String, fn(String) -> (i128, i128)> = HashMap::new();
-    // add the day1 function to the hashmap
-    daymap.insert(daystr, day1::run);
-    // call the function
-    let func = daymap.get(dayarg);
-    // dynamically get the day number from the string
-    // should not fail because we already know it's a valid day
-    let daynum = dayarg[3..].parse::<i32>().unwrap();
-    let input = read_input(daynum);
-    // if function is found, call it and print out tuple of results
-    // otherwise run invalid_input
-    match func {
-        Some(f) =>  {let (a, b) = f(input); 
-                                            println!("Part A {}\nPart B {}", a, b);},
-        None => invalid_input(dayarg),
+
+    // check if input is a valid day by comparing the first 3 chars to "day"
+    // if not, return invalid_input
+    // if it is, return the module
+    if dayarg.len() < 3 || &dayarg[..3] != "day" {
+        invalid_input(dayarg);
+        return;
+    } else {};
+
+    let daynum = dayarg[3..].parse::<i32>();
+    // match daynum and let match = daynum
+
+    match daynum {
+        Ok(n) => {
+            if n < 1 || n > 25 {
+                invalid_input(dayarg);
+                return;
+            } else {
+                // run the day
+                let input = read_input(&n);
+                //println!("input: {}", input);
+                //println!("day: {}", n);
+                match n { // add each day as a match, wanted to use macros but couldn't figure it out so write out each day
+                    1 => {
+                        
+                        let (part1, part2) = day1::run(input);
+                        println!("part1: {}", part1);
+                        println!("part2: {}", part2);
+                    },
+                    _ => {
+                        println!("Day {} not implemented yet", n);
+                    }
+                }
+            }
+        }
+        Err(_) => {
+            invalid_input(dayarg);
+            return;
+        }
     }
-    println!("Done")
 
 }
